4061ae50bdc599500143d3bf6a3d938ba9bcc3d8
Luan Cerqueira <luancerqueira.eu@gmail.com>
2023-09-04 17:17:54 -0300

    Testes com nome completo + boletim estudantil simples

--- basico-java-dio/src/basico/OlaMundo.java
+++ basico-java-dio/src/basico/OlaMundo.java
@@ -1,9 +1,29 @@
 package basico;
 
 public class OlaMundo {
+	
+	public static String nomeCompleto(String primeiroNome, String segundoNome) {
+		return primeiroNome + " " + segundoNome;
+	}
+	public static String nomeCompleto2(String primeiroNome, String segundoNome) {
+		return primeiroNome.concat(" " + segundoNome);
+	}
+	public static String nomeCompleto3(String primeiroNome, String segundoNome) {
+		return primeiroNome.concat(" ").concat(segundoNome);
+	}
+		
+	public static void main(String[] args) {
+		System.out.println("Olá mundo");
+		
+		String primeiroNome = "Luan";
+		String segundoNome ="Cerqueira";
+			
+		System.out.println(nomeCompleto(primeiroNome, segundoNome));
+			
+		String nomeCompleto2 = nomeCompleto(primeiroNome, segundoNome);
+		System.out.println(nomeCompleto2);
 
-		public static void main(String[] args) {
-			System.out.println("Olá mundo");
-			System.out.println("Luan Cerqueira");
+		String nomeCompleto3 = nomeCompleto(primeiroNome, segundoNome);
+		System.out.println(nomeCompleto3);
 	}
 }
